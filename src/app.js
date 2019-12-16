c024661fd5cc0b08c4ded983fa34ddef8c2364d2
user <user@users-MacBook-Pro-9.local>
2019-12-16 16:51:20 +0000

    add initial data (tested)

--- src/app.js
+++ src/app.js
@@ -3,5 +3,13 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
+    data: {
+      items: [
+        { event: "Shopping", status: false},
+        { event: "Gym", status: true},
+        { event: "Meeting", status: false}
+      ],
+      newItem: ""
+    }
   });
 });
